19aff9725c6c726dcfd00ac30f8f880a15871976
Veenaxworkz <veenasb.xworkz@gmail.com>
2023-04-20 12:29:28 +0530

    array programs

---
+++ Csk.java
@@ -0,0 +1,30 @@
+class Csk{
+
+public static void main(String team[]){
+
+String players[]={"MS Dhoni","Devon","Gaikwad","Ambati Rayudu","Senapati","Moeen Ali","Dube","Hangargekar","Dwaine","Santner","Jadeja","TDeshpande","Akash Singh"};
+System.out.println("CSK 11 Players are: ");
+System.out.println(players[0]);
+System.out.println(players[1]);
+System.out.println(players[2]);
+System.out.println(players[3]);
+System.out.println(players[4]);
+System.out.println(players[5]);
+System.out.println(players[6]);
+System.out.println(players[7]);
+System.out.println(players[8]);
+System.out.println(players[9]);
+System.out.println(players[10]);
+System.out.println(players[11]);
+System.out.println(players[12]);
+
+
+
+
+
+
+
+}
+
+
+}
---
+++ Div.java
@@ -0,0 +1,21 @@
+class Div{
+
+public static void main(String div[])
+{
+System.out.println("main started");
+div(43,3);
+div(67,78);
+div(876,23);
+System.out.println("main ended");
+}
+public static void div(int num1,int num2)
+{
+System.out.println("division method started");
+System.out.println(num1/num2);
+System.out.println("division method ended");
+
+
+}
+
+
+}
